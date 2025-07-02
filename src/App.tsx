e48c3238c064d795f4d95ebb48ab8bf24242b39c
Terrance Mubure <mubureterrance@gmail.com>
2025-07-02 14:25:11 +0200

    Welcome Page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Quiz from "./Components/Quiz/Quiz";
+import WelcomePage from "./Components/Welcome/WelcomePage";
 
 function App() {
   return (
-    <div>
-      <Quiz />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/quiz" element={<Quiz />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Components/Welcome/WelcomePage.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./WelcomePage.css"; // optional styling file
+
+const WelcomePage: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleStart = () => {
+    navigate("/quiz");
+  };
+
+  return (
+    <div className="welcome-container">
+      <h1>Welcome to the ACA Quiz!</h1>
+      <p>Put your knowledge to the test and see how sharp you are.</p>
+      <button onClick={handleStart}>Start Quiz</button>
+    </div>
+  );
+};
+
+export default WelcomePage;
