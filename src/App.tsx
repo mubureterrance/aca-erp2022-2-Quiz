f8133cab8551db9774da1eef49f4f03972e11897
Terrance Mubure <mubureterrance@gmail.com>
2025-07-04 10:11:23 +0200

    removed protected routed on login and register

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { AuthProvider } from "./Components/Auth/hooks/AuthProvider";
 import Quiz from "./Components/Quiz/Quiz";
 import WelcomePage from "./Components/Welcome/WelcomePage";
 import Login from "./Components/Auth/Login";
 import Register from "./Components/Auth/Register";
 import ProtectedRoute from "./Components/Auth/ProtectedRoute";
-
 
 function App() {
   return (
@@ -15,39 +19,21 @@ function App() {
         <Routes>
           {/* Public routes */}
           <Route path="/" element={<WelcomePage />} />
-          <Route 
-              path="/login" 
-              element={
-                <ProtectedRoute>
-                  <Login />
-                </ProtectedRoute>
-              } 
-            />
-            <Route 
-              path="/register" 
-              element={
-                <ProtectedRoute>
-                  <Register />
-                </ProtectedRoute>
-              } 
-            />
-          
-        {/* Protected routes that require authentication */}
-          <Route path="/" element={
-            <ProtectedRoute>
-              <WelcomePage />
-            </ProtectedRoute>
-            }/>
-          <Route path="/quiz" element={
-            <ProtectedRoute>
-              <Quiz />
-            </ProtectedRoute>
-              
-            } />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          {/* Protected routes */}
+          <Route
+            path="/quiz"
+            element={
+              <ProtectedRoute>
+                <Quiz />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </Router>
     </AuthProvider>
-    
   );
 }
 
